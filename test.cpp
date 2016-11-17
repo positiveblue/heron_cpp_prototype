6bb5ac7c3bb198ab1e44b5c16a858c34e21163f6
jms <jomsdev@gmail.com>
2016-11-17 15:53:32 -0800

    test.cpp: Compiling with Int and String working.

--- test.cpp
+++ test.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <cereal/cereal.hpp>
+
+#include "types.h"
 
 int main() {
+    Element* eInt = new Int(15);
+    Element* eString = new String("Jordi");
+    
     std::cout << "hello world" << std::endl;
 }
