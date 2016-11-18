1893bfc78351b0c3f2e9baed28d79388fff092c6
jms <jomsdev@gmail.com>
2016-11-17 16:23:36 -0800

    First version of Tuple added

---
+++ tuple/Tuple.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2015 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __HERON_TUPLE_H_
+#define __HERON_TUPLE_H_
+
+#include <sstream>
+#include "../types/types.h"
+#include "../serializer/IPluggableSerializer.h"
+
+class Tuple {
+    
+    std::map<std::string, Element* > elements;
+
+public:
+
+    Tuple() {}
+
+    Element* Get(std::string name);
+
+    void Set(std::string name, Element *e);
+
+    void serialize(IPluggableSerializer *serializer, std::stringstream &ss);
+
+};
+
+
+#endif
