a0f8d4f6e5d60fd6a1878660195818babe0fc40f
jms <jomsdev@gmail.com>
2016-11-17 16:16:31 -0800

    test.cpp updated instanciating a CerealSerializer.

--- test.cpp
+++ test.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <sstream>
 #include <cereal/cereal.hpp>
 
 #include "types.h"
+#include "CerealSerializer.h"
 
 int main() {
+    // sstream: Will contain the serialization of a tuple
+    std::stringstream ss;
+
+    // Elements
     Element* eInt = new Int(15);
     Element* eString = new String("Jordi");
+
+    // Serializer
+    CerealSerializer CSerializer;
+
+
     
+
     std::cout << "hello world" << std::endl;
 }
