e02beba754ea154ac91767cd211b0e7f20ab196a
jms <jomsdev@gmail.com>
2016-11-18 22:55:06 -0800

    test: real functional case of creating  -> saving -> loading and printing a tuple

--- test.cpp
+++ test.cpp
@@ -25,12 +25,18 @@ void readFromFile(std::string fname, std::stringstream &ss){
 }  
 
 int main() {
+    ////////////////////////
+    //* Sending a tuple *//
+    //////////////////////
+
+
     // sstream: Will contain the serialization of a tuple
-    std::stringstream ss;
+    std::stringstream os;
 
     // Elements
-    Element* eInt = new Int(15);
-    Element* eString = new String("Jordi");
+    std::shared_ptr<Element> eInt(new Int(15));
+    std::shared_ptr<Element> eDouble(new Double(3.14159));
+    std::shared_ptr<Element> eString(new String("Jordi"));
 
     // Tuple
     Tuple tuple;
@@ -38,15 +44,37 @@ int main() {
     
     tuple.Set("Worker", eString);
     tuple.Set("Salary", eInt);
-
+    tuple.Set("Phi", eDouble);
 
     // Serializer
     IPluggableSerializer *CSerializer = new CerealSerializer();
 
-    tuple.serialize(CSerializer, ss);
+    tuple.serialize(CSerializer, os);
     
+    writeToFile("serialize.out", os);
+
     std::cout << "Cross your fingers" << std::endl;
 
-    writeToFile("serialize.out", ss);
+   
+    //////////////////////////
+    //* Recieving a tuple *//
+    ////////////////////////
+
+    std::stringstream is;
+    readFromFile("serialize.out", is);
+
+    Tuple new_tuple;
+    new_tuple.deserialize(CSerializer, is);
+
+    auto Salary = 
+        std::static_pointer_cast<Int>(new_tuple.Get("Salary"));
+    auto Phi = 
+        std::static_pointer_cast<Double>(new_tuple.Get("Phi"));
+    auto Worker = 
+        std::static_pointer_cast<String>(new_tuple.Get("Worker"));
+
+    std::cout << "Salary: " << Salary->getValue() << std::endl;
+    std::cout << "Phi: " << Phi->getValue() << std::endl;
+    std::cout << "Worker: " << Worker->getValue() << std::endl;
 
 }
